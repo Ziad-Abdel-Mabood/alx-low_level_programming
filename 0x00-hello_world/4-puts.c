ffbe059bce134d41b417be3dd94f0a9ffcd7a833
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-05 21:56:54 +0000

    removed \n, fixed linting issues

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+/*
+ *Task4 of 0x00-hello_world project
+ *printing required script using puts
+ *function puts () replaces the null character with a newline or backslash n
+ */
 
+/**
+ * main - entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
-	return 0;
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
