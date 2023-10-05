ceee78aa0aef2377575f5d9cef28b16eed5dbfde
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-05 22:16:29 +0000

    task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * Task 5 
+ * print using printf() 
+ * don't forget to add the newline character
+ */
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	printf("with proper grammer, but the outcome is a piece of art,\n");
+	return (0);
+}
