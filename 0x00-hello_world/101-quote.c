5762a00942e358ae764088797e53c60604363cdd
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-06 03:51:22 +0000

    fix task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,18 +1,19 @@
 #include <stdio.h>
 /*
  * Task 8
- * printing without printf() or puts()
+ * printing without usual print functions
  * will try using an array
  */
-/*
+
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	char quote[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	
-	fwrite(quote, sizeof(quote) - 1, 1, stdout);
+	char quot[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	fwrite(quot, sizeof(quot) - 1, 1, stdout);
 	return (1);
 }
