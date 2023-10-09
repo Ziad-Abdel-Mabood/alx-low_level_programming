3ace4bbfac8de13bc0369b2e0acd8fc3d5a8cc17
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-09 19:16:11 +0000

    task 10

--- 0x01-variables_if_else_while/100-print-comb3.c
+++ 0x01-variables_if_else_while/100-print-comb3.c
@@ -1,21 +1,35 @@
 #include <stdio.h>
-/*
+/**
  * main - entry point
  *
  * Return: Always 0 (Success)
  */
-/**
+/*
  * printing combination of digits
  */
 int main(void)
 {
-	/*int i=0;
-	int j=0;*/
-	int arr1[]={0, 1, 2, 3, 4, 5, 6, 7, 8 ,9};
-	int arr2[]={0, 1, 2, 3, 4, 5, 6, 7, 8 ,9};
-	int comb1 = (arr1[8]*10) + (arr2[4]*1);
+	int i = 0;
+	int j = 0;
+	int arr1[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int arr2[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 
-	putchar(comb1);
+	for (; i < 9 && j <= 9;)
+	{
+		if (i < j)
+		{
+			putchar('0' + arr1[i]);
+			putchar('0' + arr2[j]);
+			putchar(',');
+			putchar(' ');
+		}
+		if (j == 9)
+		{
+			j = 0;
+			i++;
+		}
+		j++;
+	}
 	putchar('\n');
 	return (0);
 }
