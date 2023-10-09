acadb32ec3ebde09aa42c353ee8eaa7362e2d6cf
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-09 02:32:04 +0000

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/* print the alphabet */
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Sucess)
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar('0' + num);
+		for (num = 0; num < 9; num++)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
