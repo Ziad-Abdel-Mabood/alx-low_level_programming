5811956d4edd5d20f7e768c56fe7667f66bb7cae
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-12 15:08:12 +0000

    task 1.1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,13 +1,13 @@
 #include "main.h"
 /**
- * main - Entry point
+ * print_alphabet - a function that prints the alphabet in lower case
  *
  * Return: 0 sucess
  */
 /*
  * taks 0 : print _putchar
  */
-int main(void)
+void print_alphabet(void)
 {
 	char  alpha = 'a';
 
@@ -16,5 +16,4 @@ int main(void)
 		_putchar(alpha);
 	}
 	_putchar('\n');
-	return (0);
 }
