cfd4e01453496f157a2a1b4eeb61c4d8e0f6813b
ziad <ziadabdelmabood@gmail.com>
2023-10-13 14:29:53 +0000

    project 0x01; task 12.1

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,21 +7,28 @@
 int main(void)
 {
 	int n1 = 0;
-	int n2 = 0;
+	int n2 = 1;
 
 	for (; n1 < 99;)
 	{
-		while (n1 < n2)
+		while (n2 <= 99)
 		{
-			putchar('0' + (n1 / 10));
-			putchar('0' + (n1 % 10));
-			putchar(' ');
-			putchar('0' + (n2 / 10));
-			putchar('0' + (n2 % 10));
-			putchar(' ');
-			putchar(',');
+			if (n1 < n2)
+			{
+				putchar('0' + (n1 / 10));
+				putchar('0' + (n1 % 10));
+				putchar(' ');
+				putchar('0' + (n2 / 10));
+				putchar('0' + (n2 % 10));
+				if (n1 < 98)
+				{
+					putchar(' ');
+					putchar(',');
+				}
+			}
 			n2++;
 		}
+		n2 = 0;
 		n1++;
 	}
 	putchar('\n');
