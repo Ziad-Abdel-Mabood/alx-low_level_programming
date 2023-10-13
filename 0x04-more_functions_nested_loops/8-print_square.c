2fc4b2a572dcf250a88186b8c9d92cc98787be55
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-13 03:20:10 +0000

    task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_square - print a square
+ * @size: dimensions of square
+ * Return: void
+ */
+
+void print_square(int size)
+{
+	int h;
+	int w;
+
+	if (size > 0)
+	{
+		for (h = size; h >= 0; h--)
+		{
+			for (w = size; w >= 0; w--)
+				_putchar('#');
+			_putchar('\n');
+		}
+	} else
+		_putchar('\n');
+}
+
