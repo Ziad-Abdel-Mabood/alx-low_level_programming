99607e1f6ad4debb3bd1fb2d4352e1e47903c424
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-13 02:30:24 +0000

    task 7.3

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -14,10 +14,11 @@ void print_diagonal(int n)
 	{
 		for (i = 0; i < n; i++)
 		{
-			for (j = i; j >= 0; j--)
-				_putchar(' ');
+			for (j = i; j > 0; j--)
+				_putchar('.');
 			_putchar('\\');
 			_putchar('\n');
 		}
-	}
+	}else
+		_putchar('\n');
 }
