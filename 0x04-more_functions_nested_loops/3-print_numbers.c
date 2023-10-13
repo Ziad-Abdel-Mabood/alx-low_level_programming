10f01837d35d722c0ba3f7c231bbd2f32d77291c
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-13 01:07:59 +0000

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - print numbers 0 .. 9 then \n
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (; i < 10 ; i++)
+	{
+		_putchar('i');
+	}
+	_putchar('\n');
+}
