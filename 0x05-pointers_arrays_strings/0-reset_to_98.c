25a41d61295e8550eac84b17f6428ff118e96fd6
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-17 04:31:15 +0000

    initialized 0x05 project and wrote task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * reset_to_98 - sets argument's value to 98
+ * @n: pointer to the variable to be changed
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
