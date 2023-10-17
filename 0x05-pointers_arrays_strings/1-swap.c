8539e3cde3ae8f481e1184f581c4b0642084267f
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-17 09:08:58 +0000

    task 1; trying to push and check without compiling

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#inlcude "main.h"
+
+/**
+ * swap_int - swaps the value of two integers
+ * @a: first integer to be swapped
+ * @b: second integer to be swapped
+ * returns nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int temp;
+
+	temp = a;
+	*a = b;
+	*b = temp;
+}
