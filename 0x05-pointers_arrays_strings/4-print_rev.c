c49a6765b6cfb5a12ed66f2d3a91197b4bfc69bd
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-17 23:02:32 +0000

    0x05;task 4.0

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_rev - print a string in reverse
+ * @s: string to print
+ * Return: nothing.
+ */
+void print_rev(char *s)
+{
+	int len;
+
+	*(s + len) = 0
+	for (; len >= 0; len--)
+	{
+		_putchar(*(str + len));
+	}
+	_putchar('\n');
+}
