11a6ea38c179e47a1a192671dd6fd31b9633f5c1
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-18 04:52:01 +0000

    0x05; task 4; remove null character messing with character count

--- 0x05-pointers_arrays_strings/dbugging/4-dbug.c
+++ 0x05-pointers_arrays_strings/dbugging/4-dbug.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 /**
  * print_rev - print a string in reverse
  * @s: string to print
@@ -8,12 +9,16 @@ void print_rev(char *s)
 {
 	int len = 0;
 
-	for (; *(s + len) != 0;)
-		len++;
-	len--;
-	for (; len >= 0; len--)
+	for (; *(s + len) != 0; )
 	{
-		_putchar(*(s + len));
+		len++;
+	}
+	len--;
+	for (; len >= 0;)
+	{
+		printf("%c", *(s + len));
+		printf(" : %i, ", len);
+		len--;
 	}
 	_putchar('\n');
 }
