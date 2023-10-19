738f4302f45d9767d8f11c6172ad1235c6206335
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-19 19:43:33 +0000

    0x05; tasl 7.3

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,11 +9,12 @@ void puts_half(char *str)
 	int len;
 	int i;
 
-	for (len = 0; str[len] != 0;)
+	for (len = 0; str[len] != '\0';)
 		len++;
-	for (i = 0; i >= ((len - 1) / 2) && i < len; i++)
+	for (i = 0; i < len; i++)
 	{
-		_putchar(str[i]);
+		if (i > ((len - 1) / 2))
+			_putchar(str[i]);
 	}
 	_putchar('\n');
 }
