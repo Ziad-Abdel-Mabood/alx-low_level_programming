15f24ae3d5bcb1aa258784a6484654ab3d8efb5d
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-19 19:23:36 +0000

    0x05; task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * puts_half - print second half of the string
+ * @str: string to print
+ * Return: nothing
+ */
+void puts_half(char *str)
+{
+	int len;
+	int i;
+	int n = (l - 1) / 2
+
+	for (len = 0; str[len] != 0;)
+		len++;
+	for (i = 0; i >= n && i < len; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
