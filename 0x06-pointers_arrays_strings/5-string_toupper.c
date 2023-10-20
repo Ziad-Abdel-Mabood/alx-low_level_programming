2f7cda4bbf092ebfa1c1356c621c8ff3c523bce5
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-20 15:47:15 +0000

    0x06; task 5.0

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters of a string to uppercase
+ * Return: a character pointer that is the string in uppercase
+ */
+char *string_toupper(char *s)
+{
+	int i = 0;
+	char *src = s;
+
+	for (i = 0; *(src + i) != '\0'; i++)
+	{
+		if (*(src + i) >= 97 && *(src + i) <= 122)
+		{
+			*(src + i) = *(src + i) - 32;
+		}
+	}
+	return (src);
+}
