dba3ef6ae323bd1e9ca67fe2ec740d7f57d291b4
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-20 00:02:55 +0000

    0x06; task 2.0

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string into another with limit
+ * @dest: string that will be at the beginning
+ * @src: string that will be at the end
+ * @n: bytes to copy
+ * Return: character pointer to target string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int len_src;
+	int len_dest = 0;
+
+	for (len_src = 0; len_src < n && *(src + len_src) != '\0';)
+	{
+		*(dest + len_dest) = *(src + len_src);
+		len_dest++;
+		len_src++;
+	}
+	return (dest);
+}
