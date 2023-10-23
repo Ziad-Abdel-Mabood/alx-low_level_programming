7eca2ad6557dc7f139b74129b7e1f3837a512ba4
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-23 21:05:42 +0000

    0x07; task 1.0

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,16 @@
+include "main.h"
+/**
+ * _memcpy -	copies n bytes from memeory area src to memory area dest
+ * @dest:	destination to copy elements
+ * @src:	source of data to copy
+ * @n:		number of bytes to copy
+ * Return:	character pointer to copied elements
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		*(dest + i) = *(src + i);
+	return (dest);
+}
