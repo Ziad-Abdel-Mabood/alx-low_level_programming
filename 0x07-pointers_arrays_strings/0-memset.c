475eb6d62ebd474ba6b7c84b31a1d847a131fa63
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-23 20:52:00 +0000

    0x07; task 0.0; fix style

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,10 +1,10 @@
 #include "main.h"
 /**
- * _memset - fills first 'n' bytes of the memory area pointed to by s
- * 	     with constant byte 'b'
- * @s: pointer to memory
- * @b: byte to fill memory
- * @n: number of bytes to fill
+ * _memset -	fills first 'n' bytes of the memory area pointed to by s
+ *		with constant byte 'b'
+ * @s:	pointer to memory
+ * @b:	byte to fill memory
+ * @n:	number of bytes to fill
  * Return: pointer to memory area
  */
 char *_memset(char *s, char b, unsigned int n)
@@ -12,8 +12,6 @@ char *_memset(char *s, char b, unsigned int n)
 	unsigned int i;
 
 	for (i = 0; i < n; i++)
-	{
-		 s[i] = b;
-	}
+		s[i] = b;
 	return (s);
 }
