efd6aaa24485372d961a739ebad13e9eacb08921
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-10-24 00:55:07 +0000

    0x07; task 4.0

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -11,7 +11,7 @@ unsigned int _strspn(char *s, char *accept)
 {
 	int i;
 	int k;
-	unsigned int counter;
+	unsigned int counter = 0;
 
 	for (i = 0; s[i] != 0; i++)
 	{
---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strpbrk -	searches a string for any of a set of bytes
+ * @s:		string to check
+ * @accept:	bytes to compare with
+ * Return:	pointer to the byte in s that matches one
+ *		of the bytes in accept, or NULL if no such
+ *		byte is found.
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i;
+	int k;
+	char *match;
+
+	for (i = 0; s[i] != 0; i++)
+	{
+		for (k = 0; accept[k] != 0; k++)
+		{
+			if (s[i] == accept[k])
+			{
+				match = &s[i];
+				break;
+			}
+		}
+		if (s[i] == accept[k])
+		{
+			break;
+		}
+		k = 0;
+	}
+	if (s[i] == 0 && s[i] != accept[k])
+		match = 0;
+	return (match);
+}
