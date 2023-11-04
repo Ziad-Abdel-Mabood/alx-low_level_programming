f46bec962b89966239881090ecf2e8b6aa80331b
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-11-04 03:30:30 +0000

    0x0C; 1.3

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,6 +15,8 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 = NULL)
 		s1 = "";
+	if (s2 = NULL)
+		s2 = "";
 /*
  * getting length of string 2, and limiting n to it
  * so that we don't get extra unneeded bytes.
