a8a8c5d984a383c4ba8d16d24e86b0378659f95d
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-11-07 02:48:16 +0000

    task 2.0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,11 +16,7 @@ typedef struct dog
 } dog;
 
 
-
-/**
- * function prototypes
- */
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif /* ifndef DOG_H */
