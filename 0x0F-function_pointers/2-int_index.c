c170ccfec7f1c6ee2512da1c510ae68a7f496239
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-11-08 21:56:41 +0000

    task 2.1

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -11,14 +11,19 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
-		return (-1);
+	if (size && array && cmp)
+	{
+		if (size <= 0)
+			return (-1);
 
-	for (i = 0; cmp(array[i]) == 0 && i < size; i++)
-		;
+		for (i = 0; cmp(array[i]) == 0 && i < size; i++)
+			;
 
-	if (cmp(array[i]) == 0 && i == size)
-		return (-1);
+		if (cmp(array[i]) == 0 && i == size)
+			return (-1);
 
-	return (i);
+		return (i);
+	}
+	else
+		return (-1)
 }
