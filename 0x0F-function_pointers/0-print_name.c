4c9aa4a657bae25b34c6289a2540b94319cb18e3
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-11-08 21:03:35 +0000

    initialize project 0x0F; task 0.0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,13 @@
+#include "function_pointers.h"
+
+/**
+ * print_name -	prints a name.
+ * @name:	string that contains the name.
+ * @f:		pointer to a function that accepts
+ *		a character pointer and returns nothing.
+ * Return:	nothing.
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
