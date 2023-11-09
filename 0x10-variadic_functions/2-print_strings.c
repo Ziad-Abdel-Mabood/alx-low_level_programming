c9621d6bcb82b4ca91513444b67ef16dce5756f8
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-11-09 22:48:37 +0000

    task 2.2

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -29,7 +29,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 			if (separator && i < n - 1)
 				printf("%s", separator);
 		}
-		printf("\n");
 		va_end(args);
 	}
+	printf("\n");
 }
