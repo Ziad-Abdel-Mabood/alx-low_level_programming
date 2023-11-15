29eeedf6e003e8cc81680614cb91dadc406a1953
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-11-15 21:17:22 +0000

    task 4.0

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main -	adds positive numbers.
+ * @argc:	number of arguments passed.
+ * @argv:	array of arguments.
+ * Return:	Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	int sum, i, j;
+
+	if (argc <= 1)
+		printf("0\n");
+	for (i = 1; argv[i]; i++)
+	{
+		for (j = 0; argv[i][j]; j++)
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+				return (printf("Error\n"), 1);
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
---
+++ 0x0A-argc_argv/test_atoi.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main -	atoi test.
+ * Return:	nothing.
+ */
+int main(void)
+{
+	int num;
+	char str[] = "57";
+
+	num = atoi(str);
+	printf("%i\n", num);
+	return (0);
+}
