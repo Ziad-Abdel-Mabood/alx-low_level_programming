350a83b401a7748ff895b4d745a0058f98ad926a
Ziad-Abdel-Mabood <ziadabdelmabood@gmail.com>
2023-11-17 02:50:55 +0000

    0x12; task 101.2

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,12 @@
 section .data
 hello: db "Hello, Holberton", 0xa, 0	; string to print
-helloLen: equ $-hello			; length of string
 
 section .text
-global main		; entry point for linker
+extern	printf
+global	main		; entry point for linker
 
 main:
-mov	rax,1		; sys_write
-mov	rdi,1		; stdout
-mov	rsi,hello	; message to write
-mov	rdx, helloLen	; message length
-syscall			; call kernel
+mov	edi, hello
+mov	eax, 0
+call	printf
 
-;end program
-mov	rax,60		;sys_exit
-mov	rdi,0		;error code 0 (success)
-syscall			;call kernel
