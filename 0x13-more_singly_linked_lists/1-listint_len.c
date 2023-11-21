ca9b76e80ef0a3d6a46e650575e259ec4ab493a4
ziad <ziadabdelmabood@gmail.com>
2023-11-21 07:37:54 -0600

    task 1.1

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -10,12 +10,11 @@ size_t listint_len(const listint_t *h)
 {
 	size_t num_nodes = 0;
 
-        while (h != NULL)
-        {
-                num_nodes++;
-                h = h->next;
-        }
-
-        return (num_nodes);
+	while (h != NULL)
+	{
+		num_nodes++;
+		h = h->next;
+	}
+	return (num_nodes);
 }
 
