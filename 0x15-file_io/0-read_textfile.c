4611f95a4831fd8ebae64684259f315984a7c6af
Ziad-Abdel-Mabood <ziadabdlemabood@gmail.com>
2023-11-30 05:29:13 +0200

    t0.4; fix requested bytes for write and its return value to be equal to read bytes (before EOF)

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * read_textfile -	reads a textfile and prints it to the
@@ -11,25 +12,30 @@ ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t prnt = 0, rd;
 	int file_desc;
-	char *buffer = malloc(letters);
+	char *buffer;
 
-	if (buffer == NULL || !letters || !filename)
+	if (/*!letters ||*/ !filename)
+		return (0);
+
+	buffer = malloc(letters);
+	if (buffer == NULL)
 		return (0);
 
 	file_desc = open(filename, O_RDONLY);
-	if (file_desc < 0 || filename == NULL)
+	if (file_desc < 0)
 		return (0);
 
-	while ((rd = read(file_desc, buffer, letters)) > 0)
+	while ((rd = read(file_desc, buffer, letters)) != 0)
 	{
 		buffer[rd] = '\0';
 		if (rd < 0)
 			return (0);
+		prnt = write(1, buffer, rd);
+		if (prnt < 0 || prnt != rd)
+			return (0);
 	}
 
-	prnt = write(1, buffer, letters);
-	if (prnt < 0 || prnt != (ssize_t)letters)
-		return (0);
+	printf("rd = %ld\nprnt = %ld\n", rd, prnt);
 
 	free(buffer);
 	close(file_desc);
