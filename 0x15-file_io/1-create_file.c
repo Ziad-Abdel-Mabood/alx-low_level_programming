06d66f3a8279a1252ba4213a9ed9a8e561d9f120
ziad <ziadabdelmabood@gmail.com>
2023-12-01 09:27:09 -0600

    t1.3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,13 +9,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int len, fd;
-	ssize_t rd;
+	int len = 0, fd;
+	ssize_t rd = 0;
 
-	if (!filename || !text_content)
+	if (!filename)
 		return (-1);
 
-	for(len = 0; text_content[len]; len++)
+	for (len = 0; text_content[len]; len++)
 		;
 
 	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
