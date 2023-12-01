530629dba346fb9426213a8990436edf5ff85437
ziad <ziadabdelmabood@gmail.com>
2023-12-01 11:09:56 -0600

    t2.1

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -25,6 +25,9 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (len)
 		rd = write(fd, text_content, len);
 
+	if (rd != len)
+		return (-1);
+
 	close(fd);
 	return (1);
 }
