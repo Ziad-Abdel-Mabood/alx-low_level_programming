99724ab699b41dbe79b43e70adb31f2e4ded8928
ziad <ziadabdelmabood@gmail.com>
2024-01-12 03:42:56 +0200

    [proj 0x17][task 1] write task 1

--- 0x17-doubly_linked_lists/1-dlistint_len.c
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -1,3 +1,18 @@
 #include "lists.h"
 
 /**
+ * size_t dlistint_len(const dlistint_t *h);
+ * @h:	list header.
+ * Return:	number of elements in a linked list.
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t i = 0;
+
+	while (h)
+	{
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
